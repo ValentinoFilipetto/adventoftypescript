72c34647fc9ee9bb8f2053a9adffbf77c76c4b40
Valentino Filipetto <valefilipetto@gmail.com>
2023-12-22 10:09:31 +0100

    chore: create project and add a bunch of challenges

---
+++ src/day01/day01.ts
@@ -0,0 +1,11 @@
+import { Equal, Expect } from "../../testing-types";
+
+/* Solution */
+
+type SantasFavoriteCookies = "ginger-bread" | "chocolate-chip";
+
+/* Tests */
+type test_0_actual = SantasFavoriteCookies;
+type test_0_expected = "ginger-bread" | "chocolate-chip";
+// ^?
+type test_0 = Expect<Equal<test_0_actual, test_0_expected>>;
